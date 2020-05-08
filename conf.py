e57705c8d6bfc4b197fabe17c4822ab8a4e2c985
宋典灿 <songdiancan@xindong.com>
2020-05-08 16:44:52 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -19,13 +19,14 @@ enable_jsdelivr = {
 }
 
 # 站点设置
-site_name = "SanmingMrX"
-site_logo = "${static_prefix}logo.png"
+site_name = "只听一种声音，中国好声音。"
+#site_logo = "${static_prefix}/static/logo.png"
+site_logo = "./static/logo.png"
 site_build_date = "2019-12-18T16:51+08:00"
 author = "鸡毛土人"
 email = "songdiancan@gmail.com"
 author_homepage = "https://www.imalan.cn"
-description = "只听一种声音，中国好声音。"
+description = "凡我所爱者，必如热血中的病毒，在人群中感染流布。--by黄章晋"
 key_words = ['Maverick', '熊猫小A', 'Galileo', 'blog']
 language = 'zh-CN'
 external_links = [
